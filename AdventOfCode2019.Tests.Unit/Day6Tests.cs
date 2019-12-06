c70d780650dfdee754440deaef4a48e3c6e66bbb
Unknown <paul.cunningham@pumpkin-games.net>
2019-12-06 20:53:53 +0000

    Day6 part 2

--- AdventOfCode2019.Tests.Unit/Day6Tests.cs
+++ AdventOfCode2019.Tests.Unit/Day6Tests.cs
@@ -6,11 +6,35 @@ namespace AdventOfCode2019.Tests.Unit
     [TestFixture]
     public class Day6Tests
     {
+        //     G - H          J - K - L
+        //    /              /
+        //COM - B - C - D - E - F
+        //               \
+        //                I
+
         [TestCase("COM)B,B)C,C)D,D)E,E)F,B)G,G)H,D)I,E)J,J)K,K)L", 42)]
         public void Part1_WhenGivenInput_ReturnsOutput(string puzzleInput, int expectedValue)
         {
             var target = new Day6();
             var result = target.Part1(puzzleInput.Split(new [] {','}, StringSplitOptions.None));
+
+            Assert.AreEqual(expectedValue, result);
+        }
+
+        //                          YOU
+        //                         /
+        //        G - H       J - K - L
+        //       /           /
+        //COM - B - C - D - E - F
+        //               \
+        //                I - SAN
+
+        [TestCase("COM)B,B)C,C)D,D)E,E)F,B)G,G)H,D)I,E)J,J)K,K)L,K)YOU,I)SAN", 4)]
+        public void Part2_WhenGivenInput_ReturnsOutput(string puzzleInput, int expectedValue)
+        {
+            var input = puzzleInput.Split(new[] { ',' }, StringSplitOptions.None);
+            var target = new Day6();
+            var result = target.Part2(input, "YOU", "SAN");
 
             Assert.AreEqual(expectedValue, result);
         }
--- AdventOfCode2019/Day6.cs
+++ AdventOfCode2019/Day6.cs
@@ -13,12 +13,15 @@ namespace AdventOfCode2019
 
             var result = Part1(puzzleInput);
             Console.WriteLine($"{nameof(Day6)} {nameof(Part1)} Result: {result}");
+
+            result = Part2(puzzleInput, "YOU", "SAN");
+            Console.WriteLine($"{nameof(Day6)} {nameof(Part2)} Result: {result}");
         }
 
         public int Part1(IEnumerable<string> lines)
         {
             var parents = new Dictionary<string, List<string>>();
-            
+
             //Parse the file
             foreach (var line in lines)
             {
@@ -39,22 +42,71 @@ namespace AdventOfCode2019
             return parents.Keys.Sum(key => Count(parents, key));
         }
 
+        public int Part2(IEnumerable<string> lines, string source, string target)
+        {
+            var parents = new Dictionary<string, List<string>>();
+            var children = new Dictionary<string, string>();
+
+            //Parse the file
+            foreach (var line in lines)
+            {
+                var items = line.Split(')');
+                var parent = items[0];
+                var child = items[1];
+
+                if (!parents.ContainsKey(parent))
+                {
+                    parents.Add(parent, new List<string>());
+                }
+
+                parents[parent].Add(child);
+                children[child] = parent;
+            }
+
+            var sourceRoute = GetRouteToCentreOfMass(children, source);
+            var targetRoute = GetRouteToCentreOfMass(children, target);
+
+            var nearestCommonAncester = sourceRoute
+                .Intersect(targetRoute)
+                .First();
+
+            var sourceIndex = sourceRoute.IndexOf(nearestCommonAncester);
+            var targetIndex = targetRoute.IndexOf(nearestCommonAncester);
+
+            var totalTransfers = sourceIndex + targetIndex;
+
+            return totalTransfers;
+        }
+
+        /// <summary>
+        /// Gets a list of all the bodies from a target to the universal Centre of Mass (COM) by traversing up the 'tree'
+        /// </summary>
+        private List<string> GetRouteToCentreOfMass(Dictionary<string, string> children, string key)
+        {
+            var route = new List<string>();
+
+            children.TryGetValue(key, out var parent);
+
+            while (parent != null)
+            {
+                route.Add(parent);
+
+                children.TryGetValue(parent, out parent);
+            }
+
+            return route;
+        }
+
+        /// <summary>
+        /// Recursively count the children for a source key (the source key is the name of a body in the universe).
+        /// </summary>
         private static int Count(IReadOnlyDictionary<string, List<string>> parents, string key)
         {
-            var count = 0;
-            
-            //If this is a leaf node it's not a parent so we can bail
-            if (!parents.ContainsKey(key))
-                return count;
+            if (!parents.TryGetValue(key, out var children))
+                return 0;
 
-            //Get children for this parent...
-            var children = parents[key];
-            //...count them...
-            count += children.Count;
-            //...recurse into this child's' children so we can also count them...
-            count += children.Sum(child => Count(parents, child));
-
-            return count;
+            //Count the children and the children of the children (until no more children)...
+            return children.Count + children.Sum(child => Count(parents, child));
         }
     }
 }
